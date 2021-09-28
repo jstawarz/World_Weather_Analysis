b04401e323989a0f85d4a8ae5453849cb67bbe6d
jstawarz <89322009+jstawarz@users.noreply.github.com>
2021-09-28 18:49:42 -0400

    Add files via upload

---
+++ Weather_Database/config.py
@@ -0,0 +1,9 @@
+weather_api_key = "d3a2d9892138dc569a931381aac6aa6e"
+
+#google API key
+
+g_key = "AIzaSyBiv9Q1zR-_XFb5BznMcazn3mcPfJYwrYk"
+
+#NYT API Key
+
+nyt_key = "otzvwVYE4GTJ0lMw1KVD1WemJXozt83w"
